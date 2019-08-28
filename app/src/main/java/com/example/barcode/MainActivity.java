1625be106a1e9cbcf7cfef44bc80f805a594d6c2
redec2 <redec22@gmail.com>
2019-08-28 08:18:47 +0500

    проверка считывания кода

--- app/src/main/java/com/example/barcode/MainActivity.java
+++ app/src/main/java/com/example/barcode/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.barcode;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.media.Image;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtView = (TextView) findViewById(R.id.txtContent);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView myimageView = (ImageView)findViewById(R.id.imgview);
+                //перевод в растровое изображение
+                Bitmap myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.qr);
+                myimageView.setImageBitmap(myBitmap);
+                //Поюраюотка кода
+                BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                    .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                    .build();
+
+                if (!barcodeDetector.isOperational()) {
+                    txtView.setText("Не работает детектор");
+                }
+
+                //Создаем кадр для передачи изображения
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+                //Так как у нас всего один код
+                Barcode thisCode = barcodes.valueAt(0);
+                txtView.setText(thisCode.rawValue);
+            }
+        });
     }
 }
