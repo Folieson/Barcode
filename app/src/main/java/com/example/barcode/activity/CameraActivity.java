43e839c4c088adae1fb743ca57f99270a3f104e2
redec2 <redec22@gmail.com>
2019-08-29 22:32:14 +0500

    Считывание из галереи

---
+++ app/src/main/java/com/example/barcode/activity/CameraActivity.java
@@ -0,0 +1,86 @@
+package com.example.barcode.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.barcode.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+//    private static final int REQUEST_TAKE_PHOTO = 1;
+//    private Uri photoURI;
+//    private String mCurrentPhotoPath;
+//    private ImageView imageView;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+//        Button photoButton = (Button) findViewById(R.id.buttonLoadPicture);
+//        imageView = (ImageView) findViewById(R.id.imgGall);
+
+//    }
+
+//    public void onPhotoClick(View view) {
+//        Log.println(Log.INFO, "test", "test");
+//        dispatchTakePictureIntent();
+//    }
+//
+//    private File createImageFile() throws IOException {
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,
+//                ".jpg",
+//                storageDir
+//        );
+//
+//        mCurrentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+//
+//    private void dispatchTakePictureIntent() {
+//
+////        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+//            }
+//            if (photoFile != null) {
+//
+//            }
+////        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            Uri uri = Uri.parse(mCurrentPhotoPath);
+//            imageView.setImageURI(uri);
+//        }
+//    }
+}
--- app/src/main/java/com/example/barcode/activity/GalleryActivity.java
+++ app/src/main/java/com/example/barcode/activity/GalleryActivity.java
@@ -2,24 +2,37 @@ package com.example.barcode.activity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.barcode.R;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class GalleryActivity extends NavBar {
+
+    ImageView imageView;
+
     private static int RESULT_LOAD_IMAGE = 1;
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery);
         Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        imageView = (ImageView)findViewById(R.id.imgGall);
         buttonLoadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -29,19 +42,45 @@ public class GalleryActivity extends NavBar {
         });
     }
 
+    //Обрабатываем результат выбора в галерее:
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-            ImageView imageView = (ImageView) findViewById(R.id.imgGall);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imageView.setImageBitmap(selectedImage);
+
+                        TextView txtView = findViewById(R.id.txtGall);
+                        //Проработка кода
+                        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                                .build();
+                        if (!barcodeDetector.isOperational()) {
+                            txtView.setText("Не работает детектор");
+                            return;
+                        }
+                        //Создаем кадр для передачи изображения
+                        Frame frame = new Frame.Builder().setBitmap(selectedImage).build();
+                        SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+                        if (barcodes.size() == 0) {
+                            txtView.setText("Код не найден");
+                            return;
+                        }
+                        //Так как у нас всего один код
+                        Barcode thisCode = barcodes.valueAt(0);
+                        txtView.setText(thisCode.rawValue);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
         }
     }
+
 }
