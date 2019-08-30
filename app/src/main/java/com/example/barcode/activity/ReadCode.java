781ddce4458988432e9ce02c5c246dde6d3e31a0
redec2 <redec22@gmail.com>
2019-08-30 11:35:06 +0500

    ReadCode

--- app/src/main/java/com/example/barcode/activity/ReadCode.java
+++ app/src/main/java/com/example/barcode/activity/ReadCode.java
@@ -13,26 +13,25 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 public class ReadCode {
 
     public ReadCode (ImageView img, Bitmap bit, TextView txt, Context cnt) {
-        TextView txtView = txt;
         //Проработка кода
         BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(cnt)
                 .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
                 .build();
         if (!barcodeDetector.isOperational()) {
-            txtView.setText("Не работает детектор");
+            txt.setText("Не работает детектор");
             return;
         }
         //Создаем кадр для передачи изображения
         Frame frame = new Frame.Builder().setBitmap(bit).build();
         SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
         if (barcodes.size() == 0) {
-            txtView.setText("Код не найден");
+            txt.setText("Код не найден");
             return;
         }
         //Так как у нас всего один код
         Barcode thisCode = barcodes.valueAt(0);
-        txtView.setText(thisCode.rawValue);
-        img.setImageBitmap(bit);
+        if (txt != null) txt.setText(thisCode.rawValue);
+        if (img != null) img.setImageBitmap(bit);
 
     }
 
